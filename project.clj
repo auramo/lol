e4e03f13d2a9aaadfd115b6d2112f56534c69a2e
Rasmus Back <rasmus.back@reaktor.fi>
2011-03-03 19:46:20 +0200

    updated ring

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lol "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [ring/ring-jetty-adapter "0.2.0"]                 
+                 [ring/ring-jetty-adapter "0.3.6"]                 
                  [org.clojure/clojure-contrib "1.2.0"]]
   :main lol.server)
