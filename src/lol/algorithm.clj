6508120364f12b387e41a63237dd264aa9c6972c
Perttu Auramo <perttu.auramo@reaktor.fi>
2011-03-04 11:53:27 +0200

    proto, untested but compilable version of algo2

--- src/lol/algorithm.clj
+++ src/lol/algorithm.clj
@@ -1,4 +1,4 @@
-(ns lol.algorithm)
+(ns lol.algorithm (:use [clojure.contrib.math]))
 
 (defn sort-by-value
   [items]
@@ -11,7 +11,6 @@
 (defn substract-from-limits
   [item limits]
   (map (fn [pair] (- (first pair) (last pair))) (map list limits (dimensions-of-item item))))
-
 
 (defn fill-knapsack
   ([items limits] (fill-knapsack items limits []))
@@ -27,6 +26,40 @@
 ;; Algo-interface looks like this always: <algoname> items limits
 (defn knapsack-algorithm1 [items limits]
   (fill-knapsack (sort-by-value items) limits))
+
+;;Stuff for algorithm 2
+
+(defn get-magic-weight [dimensions limits]
+  (map #(abs (- (first %) (last %))) (map list dimensions limits)))
+
+(defn magic-predicate [item limits]
+  (let [dimensions (dimensions-of-item item)
+        value (:value item)
+        magic-value (get-magic-weight dimensions limits)]
+      (/ magic-value value)))
+
+(defn sort-by-magic-ratio [items, limits]
+  (sort-by #(magic-predicate % limits) limits))
+
+;; Yeah, pretty much copypaste from fill-knapsack -> maybe refactoring
+;; needed later...
+(defn algo2-impl
+  ([items limits] (algo2-impl items limits []))
+  ([items limits knapsack]
+     (if (empty? items)
+       knapsack
+       (let [item (first items)
+             new-limits (substract-from-limits item limits)]
+         (if (some (fn [x] (< x 0)) new-limits)
+           (recur (rest items) limits knapsack)
+           (recur (rest items) new-limits (cons item knapsack)))))))
+  
+
+;; dimensioiden erot itseisarvo (weight[0] - limit[0]) /arvo -> mahd
+;; pieni ekaks
+;; jaetaan arvot
+(defn knapsack-algorithm2 [items limits]
+  (algo2-impl (sort-by-magic-ratio items limits) limits))
 
 (defn items-to-id-list
   [items]
