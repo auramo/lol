4d3465047164e519b7831bdfbedd7fe75a783c3b
Timo Suomela <timo.suomela@reaktor.fi>
2011-03-04 15:51:44 +0200

    Sort of working testing client

--- test/lol/client.clj
+++ test/lol/client.clj
@@ -3,14 +3,31 @@
         [lol.algorithm])
   (:require [org.danlarkin.json :as json]))
 
+(defn weight-of-item
+  [item]
+  (:weight item))
+
+(defn summed-weight
+  [items]
+  (map (fn [i] (reduce + i)) (map #(flatten %) (reduce (fn [left right] (map list left right)) (map weight-of-item items)))))
+
+(summed-weight [{:weight [1 2 3]} {:weight [4 2 4]}])
+
+(defn run-one-challenge 
+  [round challenge]
+  (let [items (:contents challenge)
+        result (run-algorithm knapsack-algorithm1 challenge)
+        value (reduce + (map #(:value %) result))]
+    (str round ": " (:name challenge) " value: " value)))
+
 (defn run-challenges
   [file]
   (let [json-data (json/decode (slurp (str "testdata/" file)))
         round (:displayName json-data)
         challenges (:challenges json-data)]
-    (map (fn [c] (str round ": " (:name c) ", items: " (map #(run-algorithm knapsack-algorithm1 (:items %)) challenges))))))
+    (map #(run-one-challenge round %) challenges)))
 
-(let [files ["round-config.json" "round2-config.json"]]
+(let [files '("round-config.json" "round2-config.json")]
   (map #(run-challenges %) files))
       
       
