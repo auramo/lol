499f5f171fd18448f39f1377ee24dbb111d4fc91
Rasmus Back <rasmus.back@reaktor.fi>
2011-03-04 14:00:55 +0200

    more tests for knapsack comparisons

--- src/lol/agent.clj
+++ src/lol/agent.clj
@@ -2,15 +2,24 @@
 
 (def knapsack (agent []))
 
+(defn knapsack-value
+  [items]
+  (reduce #(+ %1 (:value %2))  0 items))
+
 (defn max-knapsack
   [current candidate]
-  (if (> (count current) (count candidate))
+  (if (> (knapsack-value current) (knapsack-value candidate))
     current
     candidate))
 
-(defn calculate-knapsack
-  [f items limits]
-  (send-off knapsack max-knapsack (f items limits)))
+(defn update-knapsack
+  [candidate]
+  (send knapsack max-knapsack candidate))
 
-
+(defn calculate
+  [algorithm items limits]
+  (let [thread (agent [])]
+    (send thread (fn [x] (algorithm items limits)))
+    (await thread)
+    @thread))
 
---
+++ test/lol/agent_test.clj
@@ -0,0 +1,17 @@
+(ns lol.agent-test
+  (:use [clojure.test]
+    [lol.agent]))
+
+(deftest test-knapsack-value
+  (let [items [{:value 12} {:value 2} {:value 3}]]
+    (is (= 17 (knapsack-value items)))))
+
+(deftest test-max-knapsack
+  (let [candidate [{:value 5} {:value 2} {:value 3}]]
+    (is (= candidate (max-knapsack [] candidate))))
+  (let [current [{:value 12} {:value 2} {:value 3}]
+        candidate [{:value 5} {:value 2} {:value 3}]]
+    (is (= current (max-knapsack current candidate))))
+  (let [current [{:value 1} {:value 2} {:value 3}]
+        candidate [{:value 5} {:value 2} {:value 3}]]
+    (is (= candidate (max-knapsack current candidate)))))
