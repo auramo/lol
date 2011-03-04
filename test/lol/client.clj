850418844001d8d454afc0d33d6285687417b331
Timo Suomela <timo.suomela@reaktor.fi>
2011-03-04 11:54:03 +0200

    Working test client

---
+++ test/lol/client.clj
@@ -0,0 +1,17 @@
+(ns lol.client
+  (:use [lol.server]
+        [lol.algorithm])
+  (:require [org.danlarkin.json :as json]))
+
+(defn run-challenges
+  [file]
+  (let [json-data (json/decode (slurp (str "testdata/" file)))
+        round (:displayName json-data)
+        challenges (:challenges json-data)]
+    (map (fn [c] (str round ": " (:name c) ", items: " (map #(run-algorithm knapsack-algorithm1 (:items %)) challenges))))))
+
+(let [files ["round-config.json" "round2-config.json"]]
+  (map #(run-challenges %) files))
+      
+      
+
