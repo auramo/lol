6e3a2218603998a4f02f9fd101b7bbfcd5e7d5c0
Timo Suomela <timo.suomela@reaktor.fi>
2011-03-05 11:54:49 +0200

    Add helper functions for tabu optimization

--- test/lol/tabu_test.clj
+++ test/lol/tabu_test.clj
@@ -9,9 +9,23 @@
     (is (= (item-on knapsack item)
            expected))))
 
+(deftest test-item-on-for
+  (let [item {:id "4"}
+        knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"}}
+        expected {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}]
+    (is (= ((item-on-for item) knapsack)
+           expected))))
+  
 (deftest test-item-off
   (let [item {:id "2"}
         knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}
         expected {"1" {:id "1"} "3" {:id "3"} "4" {:id "4"}}]
     (is (= (item-off knapsack item)
+           expected))))
+
+(deftest test-item-off-for
+  (let [item {:id "2"}
+        knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}
+        expected {"1" {:id "1"} "3" {:id "3"} "4" {:id "4"}}]
+    (is (= ((item-off-for item) knapsack)
            expected))))
