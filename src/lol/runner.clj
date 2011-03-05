0b735652b2b8826be8cda4702d3074b03a9941da
Perttu Auramo <perttu.auramo@reaktor.fi>
2011-03-05 14:06:17 +0200

    limit bug fixed from random

---
+++ src/lol/runner.clj
@@ -0,0 +1,27 @@
+(ns lol.runner
+  (:use [lol.util]
+    [lol.algorithm]
+    [lol.agent])
+  (:require [org.danlarkin.json :as json]))
+
+(defn run-one-challenge
+  [round challenge]
+  (let [items (:contents challenge)
+        limits (:capacity challenge)
+        result (run-algorithm (agent []) greedy-algorithm challenge)
+        value (knapsack-value result)
+        weight (summed-weight result)]
+    (result-str round challenge value weight limits)))
+
+(defn run-challenges
+  [file]
+  (let [json-data (json/decode (slurp (str "testdata/" file)))
+        round (:displayName json-data)
+        challenges (:challenges json-data)]
+    (map #(run-one-challenge round %) challenges)))
+
+(defn run-rounds
+  []
+  (let [files (map #(str "round" % "-config.json") '(1 2 3 4 5 6))]
+    (map #(run-challenges %) files)))
+
