9e1cead1bb367053ad6c319cd72c62227c7e2a5d
Perttu Auramo <perttu.auramo@reaktor.fi>
2011-03-05 15:58:53 +0200

    added random-algorithm

--- src/lol/server.clj
+++ src/lol/server.clj
@@ -3,6 +3,7 @@
     [ring.adapter.jetty :only [run-jetty]]
     [clojure.contrib.duck-streams :only [slurp* reader]]
     [lol.algorithm]
+    [lol.randomalgorithm]
     [lol.runner]
     [lol.agent])
   (:require [org.danlarkin.json :as json])
@@ -36,7 +37,7 @@
     (let [body (results-as-string (run-rounds))]
       (ok-response body "text/plain"))
     (let [json (parse-json-str (input-as-str req))
-          body (items-to-id-list (run-algorithms [greedy-algorithm] json))]
+          body (items-to-id-list (run-algorithms [greedy-algorithm random-algorithm] json))]
       (ok-response (encode-to-json-str body)))))
 
 (defn -main
