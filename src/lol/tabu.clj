6ebd7282b9a4ee07a14b5226dd82ebcc72c5a3d9
Timo Suomela <timo.suomela@reaktor.fi>
2011-03-05 13:43:43 +0200

    Naive implementation of objective function for tabu optimization

--- src/lol/tabu.clj
+++ src/lol/tabu.clj
@@ -1,17 +1,30 @@
-(ns lol.tabu)
+(ns lol.tabu
+  (:use lol.util))
 
 (defn item-on
-  [knapsack item]
-  (assoc knapsack (:id item) item))
+  [mapsack item]
+  (assoc mapsack (:id item) item))
 
 (defn item-off
-  [knapsack item]
-  (dissoc knapsack (:id item)))
+  [mapsack item]
+  (dissoc mapsack (:id item)))
 
 (defn item-on-for
   [item]
-  (fn [knapsack] (item-on knapsack item)))
+  (fn [mapsack] (item-on mapsack item)))
 
 (defn item-off-for
   [item]
-  (fn [knapsack] (item-off knapsack item)))
+  (fn [mapsack] (item-off mapsack item)))
+
+(defn mapsack-value
+  [mapsack]
+  (knapsack-value (vals mapsack)))
+
+(defn objective
+   [mapsack move]
+   (let [new-mapsack (move mapsack)]
+     (if (> (mapsack-value new-mapsack)
+            (mapsack-value mapsack))
+       1
+       0)))
--- test/lol/tabu_test.clj
+++ test/lol/tabu_test.clj
@@ -4,28 +4,42 @@
 
 (deftest test-item-on
   (let [item {:id "4"}
-        knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"}}
+        mapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"}}
         expected {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}]
-    (is (= (item-on knapsack item)
+    (is (= (item-on mapsack item)
            expected))))
 
 (deftest test-item-on-for
   (let [item {:id "4"}
-        knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"}}
+        mapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"}}
         expected {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}]
-    (is (= ((item-on-for item) knapsack)
+    (is (= ((item-on-for item) mapsack)
            expected))))
   
 (deftest test-item-off
   (let [item {:id "2"}
-        knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}
+        mapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}
         expected {"1" {:id "1"} "3" {:id "3"} "4" {:id "4"}}]
-    (is (= (item-off knapsack item)
+    (is (= (item-off mapsack item)
            expected))))
 
 (deftest test-item-off-for
   (let [item {:id "2"}
-        knapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}
+        mapsack {"1" {:id "1"} "2" {:id "2"} "3" {:id "3"} "4" {:id "4"}}
         expected {"1" {:id "1"} "3" {:id "3"} "4" {:id "4"}}]
-    (is (= ((item-off-for item) knapsack)
+    (is (= ((item-off-for item) mapsack)
            expected))))
+
+(deftest test-mapsack-value
+  (let [mapsack {"1" {:value 1} "2" {:value 2} "3" {:value 3} "4" {:value 4}}]
+    (is (= (mapsack-value mapsack)
+           10))))
+
+(deftest test-objective
+  (let [item1 {:id "1" :value 1}
+        item5 {:id "5" :value 10}
+        mapsack {(:id item1) item1 "2" {:value 2} "3" {:value 3} "4" {:value 4}}]    
+    (is (= (objective mapsack (item-on-for item5))
+           1))
+    (is (= (objective mapsack (item-off-for item5))
+           0))))
