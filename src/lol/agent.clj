307cca2b0c62fd865a1c6620fcf59455ecad878e
Perttu Auramo <auramo@Macintosh.local>
2011-03-09 18:09:58 +0200

    print errors for agents in other threads

--- src/lol/agent.clj
+++ src/lol/agent.clj
@@ -26,11 +26,21 @@
         items (:contents json)]
     (calculate knapsack-agent algorithm items limits)))
 
+(defn print-agent-errors-if-any
+  [agents]
+  (doseq [agent agents]
+    (let [agent-err (agent-error agent)]
+      (if agent-err
+        (do
+          (println (str "Exception occurred in agent: " agent-err))
+          (.printStackTrace agent-err))))))
+
 (defn run-algorithms
   [algorithms json]
   (let [knapsack-agent (agent [])
         agents (map #(run-algorithm knapsack-agent % json) algorithms)
         timeout (:timeout json)]
     (apply await-for (- timeout 2000) agents)
+    (print-agent-errors-if-any agents)
     (await knapsack-agent)
     @knapsack-agent))
